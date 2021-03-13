3dbec8516ca708d114e637c2419f0851bbda40a5
VishnuteerthJoshi <pe.vishnu@gmail.com>
2021-03-13 14:25:12 +0530

    commiting the report

--- Pytest_Demos/test_fixture_demo.py
+++ Pytest_Demos/test_fixture_demo.py
@@ -13,27 +13,39 @@ def test_login():
     yield
     driver.close()
 
-
+@pytest.mark.orange
 def test_title_name(test_login):
     driver.get("https://opensource-demo.orangehrmlive.com/")
     driver.maximize_window()
     driver.implicitly_wait(10)
-    if driver.title =="OrangeHRM":
+    if driver.title =="OrangeHRM123":
         assert True
     else:
         assert False
 
+@pytest.mark.facebook
 def test_facebook(test_login):
     driver.get("https://www.facebook.com/")
     driver.maximize_window()
     driver.implicitly_wait(10)
     print(driver.title)
 
+@pytest.mark.flipkart
 def test_flipkart(test_login):
     driver.get("https://www.flipkart.com/")
     driver.maximize_window()
     driver.implicitly_wait(10)
     print("Flipkar is ready")
 
+'''def pytest_configure(config):
+    config._metadata["Project Name"]="Fixture Demo "
+    config._metadata["module Name"] = "test_fixture_demo "
+    config._metadata["Name of tester"]="Vishnu"
 
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME",None)
+    metadata.pop("Packages",None)
+    metadata.pop("Platform",None)
+    metadata.pop("Plugins",None)'''
 
